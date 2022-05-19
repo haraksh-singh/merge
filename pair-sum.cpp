943cebae32b3c8ef4e6b3bcc514bf77417e78e3d
sce1 <you@example.com>
2022-05-19 19:25:57 +0530

    file commited in branch feature

---
+++ pair-sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int test_case;
+cin >> test_case;
+int num;
+int Count=0;
+for (int i=0;i<test_case;i++) {
+int n;
+cin >> n;
+int arr[n];
+for (int i=0;i<n;i++) {
+cin >> arr[i];
+}
+cin >> num;
+for (int i=0;i<n;i++) {
+for (int j=i+1;j<n;j++) {
+if ((arr[i]+arr[j])==num) {
+Count=Count+1;
+}
+}
+}
+cout << Count;
+}
+
+
+
+}
